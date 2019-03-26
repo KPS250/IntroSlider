0b693830e5e54fa2a3a7aca09ba4099e3ec98f1e
Kiran Shinde <kpshinde25@gmail.com>
2019-03-26 10:04:37 +0530

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Kiran on 26/03/19.
+//  Copyright © 2019 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
